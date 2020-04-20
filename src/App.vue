ddd01e50a6a3530c4b73945ae5b61065d2892a33
unknown <1728918814@qq.com>
2020-04-20 15:39:13 +0800

    初始化-安装axios模块

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div id="app">
-    <h1>黑马头条内容发布系统</h1>
+    <h2>黑马头条</h2>
     <!-- 路由出口 -->
     <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
 
 <style lang="less"></style>
