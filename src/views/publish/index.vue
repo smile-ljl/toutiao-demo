e94fe933bdbaf0e2e783535c16b998b49ecbcfb6
unknown <1728918814@qq.com>
2020-04-25 20:35:28 +0800

    发布文章-表单数据绑定（文章频道处理）

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -17,17 +17,23 @@
           <el-input type="textarea" v-model="article.content"></el-input>
         </el-form-item>
         <el-form-item label="封面:">
-          <el-radio-group v-model="form.resource">
-          <el-radio label="单图"></el-radio>
-          <el-radio label="三图"></el-radio>
-          <el-radio label="无图"></el-radio>
-          <el-radio label="自动"></el-radio>
+          <el-radio-group v-model="article.cover.type">
+          <el-radio :label="1">单图</el-radio>
+          <el-radio :label="3">三图</el-radio>
+          <el-radio :label="0">无图</el-radio>
+          <el-radio :label="-1">自动</el-radio>
           </el-radio-group>
         </el-form-item>
         <el-form-item label="频道:">
-          <el-select v-model="form.region" placeholder="请选择活动区域">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
+          <el-select
+            v-model="article.channel_id"
+            placeholder="请选择活动区域">
+          <el-option
+          :label="channel.name"
+          :value="channel.id"
+          v-for="(channel, index) in channels"
+          :key="index"
+          ></el-option>
           </el-select>
         </el-form-item>
         <el-form-item>
@@ -40,39 +46,40 @@
 </template>
 
 <script>
+import { getArticleChannels } from '@/api/article'
 export default {
   name: 'PublishIndex',
   components: {},
   props: {},
   data () {
     return {
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
-      },
+      channels: [], // 文章频道列表
       article: {
         title: '',
         content: '',
         cover: {
           type: 0,
           images: []
-        }
+        },
+        channel_id: null
       }
     }
   },
   computed: {},
   watch: {},
-  created () {},
+  created () {
+    this.loadChannels()
+  },
   mounted () {},
   methods: {
     onSubmit () {
       console.log('submit!')
+    },
+    loadChannels () {
+      getArticleChannels().then(res => {
+        console.log(res)
+        this.channels = res.data.data.channels
+      })
     }
   }
 }
