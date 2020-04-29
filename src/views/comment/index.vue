1cc71d5f963349ea225cc420665e27f6705ff510
unknown <1728918814@qq.com>
2020-04-29 18:03:13 +0800

    评论管理-页面布局

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,6 +1,48 @@
 <template>
   <div class="comment-container">
-    评论管理
+    <el-card class="box-card">
+      <!-- 面包屑路径导航 -->
+      <div slot="header" class="clearfix">
+        <el-breadcrumb separator="/">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item><a href="/">评论管理</a></el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <!-- /面包屑路径导航 -->
+      <!-- 表格区域 -->
+       <el-table
+        class="table-list"
+        :data="tableData"
+        stripe
+        style="width: 100%">
+        <el-table-column
+          prop="date"
+          label="日期"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址">
+        </el-table-column>
+      </el-table>
+      <!-- /表格区域 -->
+      <!-- 分页管理 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage4"
+        :page-sizes="[100, 200, 300, 400]"
+        :page-size="100"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="400">
+      </el-pagination>
+      <!-- /分页管理 -->
+  </el-card>
   </div>
 </template>
 
@@ -10,14 +52,47 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }],
+      currentPage1: 5,
+      currentPage2: 5,
+      currentPage3: 5,
+      currentPage4: 4
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    handleSizeChange (val) {
+      console.log(`每页 ${val} 条`)
+    },
+    handleCurrentChange (val) {
+      console.log(`当前页: ${val}`)
+    }
+  }
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.table-list {
+  margin-bottom: 30px
+}
+</style>
